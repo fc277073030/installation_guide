36570710229b21ee31830ae9156a0104fd98b722
范超 <fanchao@leedarson.com>
2023-05-06 15:08:38 +0800

    init

--- main.go
+++ main.go
@@ -1,23 +1,48 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
-	"log"
 	"net/http"
+	"os/exec"
 )
-
-func Login(c *gin.Context) {
-	json := make(map[string]interface{})
-	c.BindJSON(&json)
-	log.Printf("%v", &json)
-	println(&json)
-	c.IndentedJSON(http.StatusNotFound, gin.H{
-		"body": json,
-	})
-}
 
 func main() {
 	r := gin.Default()
-	r.POST("/webhook", Login)
-	r.Run(":8080")
+
+	// 加载模板文件
+	r.LoadHTMLGlob("templates/*")
+
+	// 显示初始化安装页面
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "install.gohtml", gin.H{})
+	})
+
+	// 处理安装请求
+	r.POST("/install", func(c *gin.Context) {
+		ipAddress := c.PostForm("ipAddress")
+
+		// 执行安装命令
+		cmd := exec.Command("ping", "-c", "3", ipAddress)
+		err := cmd.Run()
+
+		if err != nil {
+			// 安装失败
+			c.JSON(http.StatusOK, gin.H{
+				"status": "error",
+				"msg":    "安装失败",
+			})
+		} else {
+			// 安装成功
+			c.JSON(http.StatusOK, gin.H{
+				"status": "success",
+				"msg":    "初始化安装完成",
+			})
+		}
+	})
+
+	// 启动HTTP服务
+	if err := r.Run(":8080"); err != nil {
+		fmt.Println("HTTP server启动失败：", err)
+	}
 }
